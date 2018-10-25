fd3976f61f150ee0b0db43f663f78aa51faa032e
Vaibhav Bhawalkar <22888661+vbhv98@users.noreply.github.com>
2018-10-25 23:45:56 +0530

    Update LRU.java

--- LRU.java
+++ LRU.java
@@ -1,15 +1,12 @@
-package vaibhavb;
-
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 class LRU {
 
     public static void main(String args[]) {
         int pages[] = {0, 9, 0, 1, 8, 1, 8, 7, 8, 7, 1, 2, 8, 2, 7, 8, 2, 8};
         int capacity = 3;
-        HashSet<Integer> frames = new HashSet<>(capacity);
+        ArrayList<Integer> frames = new ArrayList<>(capacity);
         HashMap<Integer, Integer> pageToCount = new HashMap<>();
         int page_faults = 0;
         for (int i = 0; i < pages.length; i++) {
@@ -22,23 +19,23 @@ class LRU {
             } else {
                 if (!frames.contains(pages[i])) {
                     int lru = Integer.MAX_VALUE, val = Integer.MIN_VALUE;
-                    for(Integer temp : frames) {
-                        if (pageToCount.get(temp) < lru) {
-                            lru = pageToCount.get(temp);
-                            val = temp;
+                    for (Integer page : frames) {
+                        if (pageToCount.get(page) < lru) {
+                            lru = pageToCount.get(page);
+                            val = page;
                         }
                     }
-                    frames.remove(val);
+                    frames.remove(new Integer(val));
                     frames.add(pages[i]);
                     page_faults++;
                 }
                 pageToCount.put(pages[i], i);
             }
-            System.out.print("last frame state: ");
+            System.out.print("Frame State: ");
             for (Integer item : frames) {
                 System.out.print(item + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
 
         System.out.println("Total Page Faults: " + page_faults);
