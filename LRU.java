b8bd9fc3bbf9d495248858b7beb298081374b1fc
Vaibhav Bhawalkar <22888661+vbhv98@users.noreply.github.com>
2018-10-25 22:14:24 +0530

    Update LRU.java

--- LRU.java
+++ LRU.java
@@ -9,35 +9,33 @@ class LRU {
     public static void main(String args[]) {
         int pages[] = {0, 9, 0, 1, 8, 1, 8, 7, 8, 7, 1, 2, 8, 2, 7, 8, 2, 8};
         int capacity = 3;
-        HashSet<Integer> requestStr = new HashSet<>(capacity);
+        HashSet<Integer> frames = new HashSet<>(capacity);
         HashMap<Integer, Integer> pageToCount = new HashMap<>();
         int page_faults = 0;
         for (int i = 0; i < pages.length; i++) {
-            if (requestStr.size() < capacity) {
-                if (!requestStr.contains(pages[i])) {
-                    requestStr.add(pages[i]);
+            if (frames.size() < capacity) {
+                if (!frames.contains(pages[i])) {
+                    frames.add(pages[i]);
                     page_faults++;
                 }
                 pageToCount.put(pages[i], i);
             } else {
-                if (!requestStr.contains(pages[i])) {
+                if (!frames.contains(pages[i])) {
                     int lru = Integer.MAX_VALUE, val = Integer.MIN_VALUE;
-                    Iterator<Integer> itr = requestStr.iterator();
-                    while (itr.hasNext()) {
-                        int temp = itr.next();
+                    for(Integer temp : frames) {
                         if (pageToCount.get(temp) < lru) {
                             lru = pageToCount.get(temp);
                             val = temp;
                         }
                     }
-                    requestStr.remove(val);
-                    requestStr.add(pages[i]);
+                    frames.remove(val);
+                    frames.add(pages[i]);
                     page_faults++;
                 }
                 pageToCount.put(pages[i], i);
             }
             System.out.print("last frame state: ");
-            for (Integer item : requestStr) {
+            for (Integer item : frames) {
                 System.out.print(item + " ");
             }
             System.out.println("");
